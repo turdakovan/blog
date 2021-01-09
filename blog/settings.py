9010f1f7e48355f04ceb6372c2960f5747836f57
turdakovan <random_imba@mail.ru>
2021-01-09 16:49:16 +0300

    Add model post

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog_in.apps.BlogInConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
